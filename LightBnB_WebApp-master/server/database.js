67a97f74b64ccd6f99a232a940b3967540842270
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-23 15:01:49 +0000

    add property, testing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -167,8 +167,6 @@ const getAllProperties = function(options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-  // 5
-  console.log(queryString);
 
   // 6
   return pool.query(queryString, queryParams)
@@ -183,9 +181,23 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const text = `
+  INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+`;
+  const values = [];
+
+  for (key in property) {
+    values.push(property[key]);
+  }
+
+  return pool.query(text, values)
+  .then(res => res.rows[0])
+  .catch(err => console.error('Error', err.stack))
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
